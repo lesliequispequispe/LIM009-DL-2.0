80867eae060b631998413ef1aa6ee235c89542b0
lesliequispequispe <lesliequispe2605@gmail.com>
2019-04-17 10:25:28 -0500

    falta un poco mas para terminar css

--- src/data.js
+++ src/data.js
@@ -1,10 +1,33 @@
 /* Manejo de data */
-
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
-
-const example = () => {
+/* const example = () => {
   return 'example';
 };
+window.example = example; */
+const ordenadoAscendente = (data) => {
+  let arrOrdenadoAsc = [];
+  arrOrdenadoAsc = data.sort(
+    (a, b) => {
+      let aa = parseInt(a.yearOfBirth);
+      let bb = parseInt(b.yearOfBirth);
+      if (aa < bb) {
+        return -1;
+      } else if (aa > bb) {
+        return 1;
+      } else {
+        return 0;
+      }
+    })
+  return arrOrdenadoAsc;
+}
 
-window.example = example;
+window.ordenadoAscendente = ordenadoAscendente;
+
+const filtroGenero = (data, condicion) => {
+  let arrFiltroGenero = [];
+  arrFiltroGenero = data.filter((aa) =>
+    aa.gender === condicion);
+  return arrFiltroGenero;
+}
+
+window.filtroGenero = filtroGenero;
+
--- src/main.js
+++ src/main.js
@@ -1,1 +1,42 @@
 /* Manejo del DOM */
+const cargarJSON = () => {
+    fetch("./data/potter.json")
+      .then((res) =>  {
+        return res.json();
+      })
+      .then((data) => {
+        pintadoContenedor.innerHTML = pintandoDataHarryPotter(data);
+        /* const ascendente = document.getElementById("ascendente");
+          ascendente.addEventListener("click", () => { 
+            pintadoContenedor.innerHTML = pintandoDataHarryPotter(ordenadoAscendente(data));
+            const descendente = document.getElementById("descendente");
+          descendente.addEventListener("click", () => { 
+            pintadoContenedor.innerHTML = pintandoDataHarryPotter(ordenadoAscendente(data).reverse());
+          })
+        }) */
+      })
+    }
+  cargarJSON();
+  const pintadoContenedor = document.getElementById("output");
+  const pintandoDataHarryPotter = (data) => {
+  let pintado = "";
+  for (let i = 0; i < data.length; i++) {
+     pintado += `<div class="contenedor-pintado">
+                  <div>
+                  <img  class="imagenes" src="${data[i].image}" alt="${data[i].image}"/>
+                  </div>
+                  <div>
+                  <p>name:${data[i].name}</p>
+                  <p>actor:${data[i].actor}</p>
+                  <p>species:${data[i].species}</p>
+                  <p>house:${data[i].house}</p>
+                  <p>dateOfBirth:${data[i].dateOfBirth}</p>
+                  <p>yearOfBirth:${data[i].yearOfBirth}</p>
+                  <p>hogwartsStudent:${data[i].hogwartsStudent}</p>
+                  <p>hogwartsStaff:${data[i].hogwartsStaff}</p>
+                  </div>
+                  </div>`
+  }
+ return pintado;
+}
+window.pintandoDataHarryPotter = pintandoDataHarryPotter; 
